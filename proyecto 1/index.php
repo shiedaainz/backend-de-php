84de0b981483b1bec79ee1929606e4a4863797c4
jcastillo061-admin <uajfdhrsjaj34@gmail.com>
2022-12-30 19:22:27 -0500

    primer proyecto en php

---
+++ proyecto 1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>pagina con php</title>
+    </head>
+    <body>
+        <?php
+            echo 'Juan Camilo Castillo Sanchez <br>';
+            echo 'mi numero de cedula: 1127393488 <br>';
+            echo 'hola instructor camilo enrique'
+            . ' buenas tardes soy juan camilo castillo sanchez espero aprender mucho con este curso';
+        ?>
+    </body>
+</html>
